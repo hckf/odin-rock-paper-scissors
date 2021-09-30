a220f07ff3c1bef240480186398077d363a59c4f
hckf <45421713+hckf@users.noreply.github.com>
2021-09-30 12:57:51 +0100

    Added a simple UI to the rock paper scissors game

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-function plChoice() {
-  let plChoice = prompt("Please enter either Rock, Paper or Scissors!");
-  let proceed = true;
-  let validChoices = ["rock", "paper", "scissors"];
-  while (proceed) {
-    if (typeof plChoice == "string") {
-      plChoice = plChoice.trim();
-      if (plChoice != "") {
-        if (validChoices.includes(plChoice)) {
-          proceed = false;
-          return plChoice;
-        }
-      }
-    }
-    plChoice = prompt("Be sure to select either rock, paper or scissors!");
-  }
-}
+const buttonRock = document.querySelector("#button-rock");
+const buttonPaper = document.querySelector("#button-paper");
+const buttonScissors = document.querySelector("#button-scissors");
 
+let gameScore = document.querySelector("#score");
+gameScore.textContent = "Player Score: 0  Computer Score: 0";
+let gameState = document.querySelector("#game-state");
+gameState.textContent = "Make your move!";
+
+let playerScore = 0;
+let computerScore = 0;
+
+buttonRock.addEventListener("click", () => game("rock", cpuChoice()));
+buttonPaper.addEventListener("click", () => game("paper", cpuChoice()));
+buttonScissors.addEventListener("click", () => game("scissors", cpuChoice()));
+
+// Provides the computer with a random choice of either rock, paper or scissors
 function cpuChoice() {
   let computerChoice = ["rock", "paper", "scissors"];
   let randomCPUChoice =
@@ -24,77 +23,61 @@ function cpuChoice() {
   return randomCPUChoice;
 }
 
-function game() {
-  let gameRounds = 5;
+// Main game - sets player/CPU choice and scores, and the number of rounds ('best of' x) to be played
 
-  let playerChoice;
-  let playerScore = 0;
-
-  let computerChoice;
-  let computerScore = 0;
-  for (let i = 0; i <= gameRounds; i++) {
-    playerChoice = plChoice();
-    computerChoice = cpuChoice();
-    if (computerChoice == "rock") {
-      switch (playerChoice.toLowerCase()) {
-        case "rock":
-          console.log("Both players chose rock: a tie. Please try again!");
-          i--;
-          break;
-        case "paper":
-          console.log("The computer chose rock, you win!");
-          playerScore++;
-          break;
-        case "scissors":
-          console.log("The computer chose rock, you lose!");
-          computerScore++;
-          break;
-      }
-    } else if (computerChoice == "paper") {
-      switch (playerChoice.toLowerCase()) {
-        case "paper":
-          console.log("Both players chose paper: a tie. Please try again!");
-          i--;
-          break;
-        case "scissors":
-          console.log("The computer chose paper, you win!");
-          playerScore++;
-          break;
-        case "rock":
-          console.log("The computer chose paper, you lose!");
-          computerScore++;
-          break;
-      }
-    } else if (computerChoice == "scissors") {
-      switch (playerChoice.toLowerCase()) {
-        case "scissors":
-          console.log("Both players chose scissors: a tie. Please try again!");
-          i--;
-          break;
-        case "rock":
-          console.log("The computer chose scissors, you win!");
-          playerScore++;
-          break;
-        case "paper":
-          console.log("The computer chose scissors, you lose!");
-          computerScore++;
-          break;
-      }
+function game(playerChoice, computerChoice) {
+  // If the computer chose rock
+  if (computerChoice == "rock") {
+    switch (playerChoice.toLowerCase()) {
+      case "rock":
+        gameState.textContent = "Both players chose rock: a tie!";
+        break;
+      case "paper":
+        gameState.textContent = "The computer chose rock, you win!";
+        playerScore++;
+        break;
+      case "scissors":
+        gameState.textContent = "The computer chose rock, you lose!";
+        computerScore++;
+        break;
     }
-    console.log(
-      `Player Score: ${playerScore}  Computer Score: ${computerScore}`
-    );
+    // If the computer chose paper
+  } else if (computerChoice == "paper") {
+    switch (playerChoice.toLowerCase()) {
+      case "paper":
+        gameState.textContent = "Both players chose paper: a tie!";
+        break;
+      case "scissors":
+        gameState.textContent = "The computer chose paper, you win!";
+        playerScore++;
+        break;
+      case "rock":
+        gameState.textContent = "The computer chose paper, you lose!";
+        computerScore++;
+        break;
+    }
+    // If the computer chose scissors
+  } else if (computerChoice == "scissors") {
+    switch (playerChoice.toLowerCase()) {
+      case "scissors":
+        gameState.textContent = "Both players chose scissors: a tie!";
+        break;
+      case "rock":
+        gameState.textContent = "The computer chose scissors, you win!";
+        playerScore++;
+        break;
+      case "paper":
+        gameState.textContent = "The computer chose scissors, you lose!";
+        computerScore++;
+        break;
+    }
   }
-  if (playerScore > computerScore) {
-    alert("You win!");
-  } else if (computerScore > playerScore) {
-    alert("You lose!");
-  } else {
-    alert("It's a tie!");
+  // Log scores at the end of each round
+  gameScore.textContent = `Player Score: ${playerScore}  Computer Score: ${computerScore}`;
+  // At the end of x rounds, check who has the higher score
+  if (playerScore == 5) {
+    gameState.textContent = "You win!";
+  } else if (computerScore == 5) {
+    gameState.textContent = "You lose!";
   }
-}
-if (confirm("Do you want to play Rock Paper Scissors?")) {
-  game();
-} else {
-  alert("Perhaps another time?");
 }
